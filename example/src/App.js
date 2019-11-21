3c070820fc2d056837f5a32555d22d98f8778c33
Faizah <eeza1710@gmail.com>
2019-11-21 16:30:28 +0800

    Initial Commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import ExampleComponent from 'colored-heading1'
+//import ExampleComponent from 'colored-heading1'
+import { ColoredHeadingOne, ColoredHeadingTwo } from "colored-heading1";
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ColoredHeadingOne
+          text="Colored Heading One is working"
+          color={"red"}
+        />
+
+        <ColoredHeadingTwo
+          text="Colored Heading Two is working"
+          color={"blue"}
+        />
       </div>
-    )
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import styles from './styles.css'
+// import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
+export class ColoredHeadingOne extends Component {
   static propTypes = {
     text: PropTypes.string
-  }
+  };
 
   render() {
-    const {
-      text
-    } = this.props
+    const { text, color } = this.props;
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+    //return <div className={styles.test}>Example Component: {text}</div>;
+    return <h1 style={{ color }}> {text}</h1>;
+  }
+}
+
+export class ColoredHeadingTwo extends Component {
+  static propTypes = {
+    text: PropTypes.string
+  };
+
+  render() {
+    const { text, color } = this.props;
+
+    //return <div className={styles.test}>Example Component: {text}</div>;
+    return <h2 style={{ color }}> {text}</h2>;
   }
 }
